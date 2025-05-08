3f5945acc143506063ed8255375f94b80d31d737
wilson-0302 <gksmf030302@handong.ac.kr>
2025-05-09 00:15:40 +0900

    jump

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
     public float maxSpeed;
+    public float jumpPower;
     Rigidbody2D rigid;
     SpriteRenderer spriteRenderer;
     Animator anim;
@@ -19,17 +20,29 @@ public class PlayerMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //stop speed
-        if(Input.GetButtonUp("Horizontal")) {
-            rigid.velocity = new Vector2(rigid.velocity.normalized.x * 0.5f, rigid.velocity.y);
+        //jump
+        if (Input.GetButtonDown("Jump") && !anim.GetBool("isJump")){
+            rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+            anim.SetBool("isJump", true);
         }
-        
+
+        //stop speed
+        // if (Input.GetButtonUp("Horizontal"))
+        // {
+        //     rigid.velocity = new Vector2(rigid.velocity.normalized.x * 0.5f, rigid.velocity.y);
+        // }
+        float deceleration = 0.9f;
+        if (Input.GetAxisRaw("Horizontal") == 0){
+            rigid.velocity = new Vector2(rigid.velocity.x * deceleration, rigid.velocity.y);
+        }
+
         //direction sprite
-        if(Input.GetButtonDown("Horizontal")){
+        if (Input.GetAxisRaw("Horizontal") != 0){
             spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") == -1;
         }
 
-        if(Mathf.Abs(rigid.velocity.x) < 0.3){
+        //animation
+        if (Mathf.Abs(rigid.velocity.x) < 0.3){
             anim.SetBool("isWalk", false);
         }
         else{
@@ -39,18 +52,31 @@ public class PlayerMove : MonoBehaviour
 
     void FixedUpdate()
     {
+        //move speed
         float h = Input.GetAxisRaw("Horizontal");
         rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
 
-        if(rigid.velocity.x > maxSpeed)
-        rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
-        else if(rigid.velocity.x < maxSpeed * -1)
-        rigid.velocity = new Vector2(maxSpeed * -1, rigid.velocity.y);
+        //max speed
+        if (rigid.velocity.x > maxSpeed)
+            rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
+        else if (rigid.velocity.x < maxSpeed * -1)
+            rigid.velocity = new Vector2(maxSpeed * -1, rigid.velocity.y);
+
+        //landing platform
+        if(rigid.velocity.y < 0){
+            Debug.DrawRay(rigid.position, Vector3.down, new Color(0, 1, 0));
+            RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, Vector3.down, 1, LayerMask.GetMask("Platform"));
+            if(rayHit.collider != null){
+                if(rayHit.distance < 1f){
+                    anim.SetBool("isJump", false);
+                }
+            }
+        }
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 }
